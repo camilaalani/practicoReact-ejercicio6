1ad98eff1159d74f20647b256358589c2569a9ef
Camila Alani <alanicamila124@gmail.com>
2024-01-28 04:33:03 -0300

    agregue un formulario dentro de la card para que el usuario pueda ingresar el color a traves del input, el boton para que pueda dar de alta los colores y un alert para los mensajes de error

--- src/components/SelectorDeColores.jsx
+++ src/components/SelectorDeColores.jsx
@@ -10,6 +10,27 @@ const SelectorDeColores = () => {
             <Card>
         <Card.Body className="d-flex flex-column">
           <Card.Title className="text-center">Administrar Colores</Card.Title>
+          <Form className="w-100">
+   <Form.Group controlId="colorPersonalizado" className="mb-2">
+     <Form.Label>Ingrese un color:</Form.Label>
+     <Form.Control
+       type="text"
+       placeholder="por ejemplo, azul, rojo, verde"
+       value={nombreColor}
+       onChange={manejarCambioDeColor}
+       onKeyPress={manejarKeyPress}
+     />
+   </Form.Group>
+   <Button
+     variant="primary"
+     type="button"
+     className="mt-2 align-self-end"
+     onClick={manejarGuardarColor}
+   >
+     Guardar
+   </Button>
+</Form>
+{mensajeError && <Alert variant="danger" className="mt-2 mb-2">{mensajeError}</Alert>}
         </Card.Body>
       </Card>
         <section className="d-flex flex-wrap mt-3">
