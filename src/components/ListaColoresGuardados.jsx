3d972b4343b06d71c4236ba16e6acba41ac0488b
Camila Alani <alanicamila124@gmail.com>
2024-01-28 04:57:19 -0300

    agregue un map del array coloresGuardados para renderizar cada color de la card y una card donde aparece el color dado de alta por el usuario, con el nombre del color y un boton para borrar la card con el color

--- src/components/ListaColoresGuardados.jsx
+++ src/components/ListaColoresGuardados.jsx
@@ -1,7 +1,20 @@
-const ListaColoresGuardados = () => {
+import { Card, Button } from 'react-bootstrap';
+
+const ListaColoresGuardados = ({ coloresGuardados, manejarBorrarColor }) => {
   return (
     <>
-
+    {coloresGuardados.map((color) => (
+        <Card key={color.nombre} className="m-2">
+          <Card.Body style={{ backgroundColor: color.codigo }}>
+            <Card.Title>{color.nombre}</Card.Title>
+          </Card.Body>
+          <Card.Footer>
+            <Button variant="danger" onClick={() => manejarBorrarColor(color.nombre)}>
+              Borrar
+            </Button>
+          </Card.Footer>
+        </Card>
+      ))}
       </>
   );
 };
