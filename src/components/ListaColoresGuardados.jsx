d11adc9e5010c7b19903d4f614077cb1da1e3bfc
Camila Alani <alanicamila124@gmail.com>
2024-01-28 04:58:22 -0300

    le di formato a los componentes con prettier

--- src/components/ListaColoresGuardados.jsx
+++ src/components/ListaColoresGuardados.jsx
@@ -1,21 +1,24 @@
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button } from "react-bootstrap";
 
 const ListaColoresGuardados = ({ coloresGuardados, manejarBorrarColor }) => {
   return (
     <>
-    {coloresGuardados.map((color) => (
+      {coloresGuardados.map((color) => (
         <Card key={color.nombre} className="m-2">
           <Card.Body style={{ backgroundColor: color.codigo }}>
             <Card.Title>{color.nombre}</Card.Title>
           </Card.Body>
           <Card.Footer>
-            <Button variant="danger" onClick={() => manejarBorrarColor(color.nombre)}>
+            <Button
+              variant="danger"
+              onClick={() => manejarBorrarColor(color.nombre)}
+            >
               Borrar
             </Button>
           </Card.Footer>
         </Card>
       ))}
-      </>
+    </>
   );
 };
 
--- src/components/SelectorDeColores.jsx
+++ src/components/SelectorDeColores.jsx
@@ -1,32 +1,31 @@
-import React, { useState } from 'react';
-import { Container, Form, Card, Button, Alert } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Container, Form, Card, Button, Alert } from "react-bootstrap";
 import ListaColoresGuardados from "./ListaColoresGuardados";
 
 const nombresColores = {
-    azul: '#0000FF',
-    rojo: '#FF0000',
-    verde: '#008000',
-    rosa: "#FF689D",
-    amarillo: "#FFFB00",
-    naranja: "#FF8000",
-    morado: "#7000FF",
-    marron: "#553D13",
-    blanco: "#FFFFFF",
-    negro: "#000000",
-    celeste: "#00A6FF",
-    gris: "#9E9E9E",
-  };
-
+  azul: "#0000FF",
+  rojo: "#FF0000",
+  verde: "#008000",
+  rosa: "#FF689D",
+  amarillo: "#FFFB00",
+  naranja: "#FF8000",
+  morado: "#7000FF",
+  marron: "#553D13",
+  blanco: "#FFFFFF",
+  negro: "#000000",
+  celeste: "#00A6FF",
+  gris: "#9E9E9E",
+};
 
 const SelectorDeColores = () => {
-    const [nombreColor, setNombreColor] = useState('');
+  const [nombreColor, setNombreColor] = useState("");
   const [coloresGuardados, setColoresGuardados] = useState(
-    JSON.parse(localStorage.getItem('coloresGuardados')) || []
+    JSON.parse(localStorage.getItem("coloresGuardados")) || []
   );
-  const [mensajeError, setMensajeError] = useState(''); 
+  const [mensajeError, setMensajeError] = useState("");
 
   const manejarCambioDeColor = (e) => {
-    setMensajeError('');
+    setMensajeError("");
 
     const valorInput = e.target.value.toLowerCase();
     setNombreColor(valorInput);
@@ -34,19 +33,23 @@ const SelectorDeColores = () => {
 
   const manejarGuardarColor = () => {
     if (!/^[a-zA-Z]+$/.test(nombreColor)) {
-      setMensajeError('Por favor, ingresa solo letras para el nombre del color.');
+      setMensajeError(
+        "Por favor, ingresa solo letras para el nombre del color."
+      );
       return;
     }
 
     if (nombreColor.length < 2 || nombreColor.length > 15) {
-      setMensajeError('Por favor, ingresa un color con más de una letra y menos de 15 letras.');
+      setMensajeError(
+        "Por favor, ingresa un color con más de una letra y menos de 15 letras."
+      );
       return;
     }
 
     const codigoColor = nombresColores[nombreColor];
 
     if (!codigoColor) {
-      setMensajeError('El color ingresado no está disponible.');
+      setMensajeError("El color ingresado no está disponible.");
       return;
     }
 
@@ -56,10 +59,13 @@ const SelectorDeColores = () => {
     };
 
     const nuevosColoresGuardados = [...coloresGuardados, nuevoColor];
-    localStorage.setItem('coloresGuardados', JSON.stringify(nuevosColoresGuardados));
+    localStorage.setItem(
+      "coloresGuardados",
+      JSON.stringify(nuevosColoresGuardados)
+    );
     setColoresGuardados(nuevosColoresGuardados);
 
-    setNombreColor('');
+    setNombreColor("");
   };
 
   const manejarBorrarColor = (nombre) => {
@@ -67,51 +73,60 @@ const SelectorDeColores = () => {
       (color) => color.nombre !== nombre
     );
 
-    localStorage.setItem('coloresGuardados', JSON.stringify(nuevosColoresGuardados));
+    localStorage.setItem(
+      "coloresGuardados",
+      JSON.stringify(nuevosColoresGuardados)
+    );
     setColoresGuardados(nuevosColoresGuardados);
   };
 
   const manejarKeyPress = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       e.preventDefault();
       manejarGuardarColor();
     }
   };
 
-    return (
-        <Container className="mt-5">
-            <Card>
+  return (
+    <Container className="mt-5">
+      <Card>
         <Card.Body className="d-flex flex-column">
           <Card.Title className="text-center">Administrar Colores</Card.Title>
           <Form className="w-100">
-   <Form.Group controlId="colorPersonalizado" className="mb-2">
-     <Form.Label>Ingrese un color:</Form.Label>
-     <Form.Control
-       type="text"
-       placeholder="por ejemplo, azul, rojo, verde"
-       value={nombreColor}
-       onChange={manejarCambioDeColor}
-       onKeyPress={manejarKeyPress}
-     />
-   </Form.Group>
-   <Button
-     variant="primary"
-     type="button"
-     className="mt-2 align-self-end"
-     onClick={manejarGuardarColor}
-   >
-     Guardar
-   </Button>
-</Form>
-{mensajeError && <Alert variant="danger" className="mt-2 mb-2">{mensajeError}</Alert>}
+            <Form.Group controlId="colorPersonalizado" className="mb-2">
+              <Form.Label>Ingrese un color:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="por ejemplo, azul, rojo, verde"
+                value={nombreColor}
+                onChange={manejarCambioDeColor}
+                onKeyPress={manejarKeyPress}
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="button"
+              className="mt-2 align-self-end"
+              onClick={manejarGuardarColor}
+            >
+              Guardar
+            </Button>
+          </Form>
+          {mensajeError && (
+            <Alert variant="danger" className="mt-2 mb-2">
+              {mensajeError}
+            </Alert>
+          )}
         </Card.Body>
       </Card>
-        <section className="d-flex flex-wrap mt-3">
-            <ListaColoresGuardados coloresGuardados={coloresGuardados}
-          manejarBorrarColor={manejarBorrarColor}></ListaColoresGuardados>
-        </section>
-        </Container>
-    );
+      <section className="d-flex flex-wrap mt-3">
+        <ListaColoresGuardados
+          coloresGuardados={coloresGuardados}
+          manejarBorrarColor={manejarBorrarColor}
+        ></ListaColoresGuardados>
+      </section>
+    </Container>
+  );
 };
 
 export default SelectorDeColores;
